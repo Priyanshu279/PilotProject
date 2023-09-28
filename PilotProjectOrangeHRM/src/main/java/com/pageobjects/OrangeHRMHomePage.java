edea5da68b8e9ccfaf2ef7ef146486b590e7d851
Priyanshu Suryavanshi <86827550+Priyanshu279@users.noreply.github.com>
2023-09-28 23:12:11 +0530

    Delete PilotProjectOrangeHRM directory

--- PilotProjectOrangeHRM/src/main/java/com/pageobjects/OrangeHRMHomePage.java
+++
@@ -1,9 +0,0 @@
-package com.pageobjects;
-
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-public class OrangeHRMHomePage {
-	@FindBy(xpath="//h6[text()='Dashboard']")
-	public static WebElement homePage;
-}
