2a84ffb1cda3ef04ca400c21fb45f4c4031288ef
Mallikarjun-Maralinganavar <141898201+Mallikarjun-Maralinganavar@users.noreply.github.com>
2023-09-28 12:28:06 +0530

    Update 2.1
    Update 2.1

---
+++ PilotProjectOrangeHRM/src/main/java/com/pageactions/Recruitment_CandiatesActions.java
@@ -0,0 +1,10 @@
+package com.pageactions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Recruitment_CandiatesActions {
+
+	
+}
